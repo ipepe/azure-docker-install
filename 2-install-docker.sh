1b319838f136fb7dd0190670228569af13bea382
Patryk Ptasiński <github@ipepe.pl>
2020-04-10 14:58:34 +0200

    Update 2-install-docker.sh

--- 2-install-docker.sh
+++ 2-install-docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 echo Installing docker...
 sudo apt-get update
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
