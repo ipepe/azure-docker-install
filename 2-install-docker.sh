37a12305886d253a938dfbbfc0f958216af7d677
Patryk Ptasiński <github@ipepe.pl>
2020-04-30 16:21:29 +0200

    Update 2-install-docker.sh

--- 2-install-docker.sh
+++ 2-install-docker.sh
@@ -4,10 +4,7 @@ set -e
 
 echo Installing docker...
 sudo apt-get update
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-sudo apt-get update
-sudo apt-get install -y docker.io
+sudo apt install docker.io
 
 #status of docker
 echo You can check status of docker service with:
